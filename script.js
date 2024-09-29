08b27c708da3f4773b0a33cb8bba639123106e7c
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-29 13:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.getElementById('removeButton').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+    // Remove the selected option
+    if (selectedOption) {
+        colorSelect.removeChild(selectedOption);
+    }
+});
